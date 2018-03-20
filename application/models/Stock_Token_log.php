51bacb8a185bd7e6c13fc9544ad3330942275a84
firoz baksh <ferozbaksh3191@gmail.com>
2018-03-20 14:59:05 +0530

    Stock token updated when api authenticated

--- application/models/Stock_Token_log.php
+++ application/models/Stock_Token_log.php
@@ -12,7 +12,11 @@ class Stock_Token_log extends CI_Model
 		{
 			date_default_timezone_set('Asia/Kolkata');  
 			$date = date('Y-M-d h:i:s A', time());
-			$this->db->insert('stock_api', ['vendor_id' => $user_id, 'api_key' => '3tAI6YEbd48Hqw5zEV2YM9brEDNaps4V7dRcblQM', 'access_token' => $access_token, 'date' => $date]);
+			$data = ['vendor_id' => $user_id, 'api_key' => '3tAI6YEbd48Hqw5zEV2YM9brEDNaps4V7dRcblQM', 'access_token' => $access_token, 'date' => $date, 'is_active' => 1];
+			
+			return $this->db
+						->where("vendor_id", $user_id)
+						->update("stock_api", $data);
 		}
 		
 	}
